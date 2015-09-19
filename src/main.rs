d3559853a34735a836853bc0eacacab550b2524a
Gio d'Amelio <giodamelio@gmail.com>
2015-09-18 22:35:16 -0700

    Parse line with nom

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,71 @@
+#[macro_use]
+extern crate nom;
+
 use std::error::Error;
+use std::str;
 use std::fs::File;
 use std::io::BufReader;
 use std::io::BufRead;
 use std::path::Path;
 
-fn main() {
-    // Create a path to the desired file
-    let path = Path::new("test_data/ipfs.log");
-    let display = path.display();
+use nom::{not_line_ending};
 
-    // Open the path in read-only mode, returns `io::Result<File>`
+fn main() {
+    // Path to the log file
+    let path = Path::new("test_data/ipfs.log");
+
+    // Parse the log file
+    parse(path);
+}
+
+fn parse(path: &Path) {
+    // Open file
     let file = match File::open(&path) {
-        // The `description` method of `io::Error` returns a string that describes the error
-        Err(why) => panic!("couldn't open {}: {}", display, Error::description(&why)),
+        Err(why) => panic!("Couldn't open {}: {}", path.display(), Error::description(&why)),
         Ok(file) => file,
     };
 
-    // Collect all lines into a vector
-    let reader = BufReader::new(file);
+    // Create our parser
+    #[derive(Debug)]
+    struct Line<'a> {
+        timestamp: &'a str,
+        username: &'a str,
+        message: &'a str,
+    }
 
+    named!(parse_line<&[u8], Line>,
+        chain!(
+            timestamp: map_res!(
+                take_until_and_consume!("\t"),
+                str::from_utf8
+            ) ~
+            username: map_res!(
+                take_until_and_consume!("\t"),
+                str::from_utf8
+            ) ~
+            message: map_res!(
+                not_line_ending,
+                str::from_utf8
+            ) ,
+
+            || {
+                Line {
+                    timestamp: timestamp,
+                    username: username,
+                    message: message,
+                }
+            }
+        )
+    );
+
+    // Iterate over lines
+    let reader = BufReader::new(file);
     for l in reader.lines() {
-        println!("{}", l.unwrap());
+        let line = match l {
+            Err(why) => panic!("Couldn't get line: {}", Error::description(&why)),
+            Ok(line) => line,
+        };
+
+        println!("{:?}", parse_line(line.as_bytes()));
     }
 }
