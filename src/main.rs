3f773085393ebb9ca77d5ea7e8f1580ff2b4b257
Gio d'Amelio <giodamelio@gmail.com>
2015-09-18 20:39:46 -0700

    Iterate over log file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::BufRead;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    // Create a path to the desired file
+    let path = Path::new("test_data/ipfs.log");
+    let display = path.display();
+
+    // Open the path in read-only mode, returns `io::Result<File>`
+    let file = match File::open(&path) {
+        // The `description` method of `io::Error` returns a string that describes the error
+        Err(why) => panic!("couldn't open {}: {}", display, Error::description(&why)),
+        Ok(file) => file,
+    };
+
+    // Collect all lines into a vector
+    let reader = BufReader::new(file);
+
+    for l in reader.lines() {
+        println!("{}", l.unwrap());
+    }
 }
